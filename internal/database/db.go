76eb76173869f00e887c71d3638019f6218acf89
Black-Sirius69 <vedant.matanhelia@gmail.com>
2024-02-05 11:18:23 +0530

    feat: postgres connection

--- internal/database/db.go
+++ internal/database/db.go
@@ -2,11 +2,25 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/CodeChefVIT/devsoc-backend-24/config"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 var DB *sql.DB
 
 func InitDB(dbConfig config.DatabaseConfig) {
+	var err error
+	uri := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbConfig.DBHost, dbConfig.DBPort, dbConfig.DBUserName, dbConfig.DBUserPassword, dbConfig.DBName)
+	DB, err = sql.Open("pgx", uri)
+	if err != nil {
+		slog.Error("Failed to connect to database with error: " + err.Error())
+		slog.Info("The connection string used was: " + uri)
+		os.Exit(1)
+	}
 }
