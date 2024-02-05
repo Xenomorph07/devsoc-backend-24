b2937a14dd1b9c68e7c1b9adc1aae4247d227e49
Black-Sirius69 <vedant.matanhelia@gmail.com>
2024-02-05 11:12:53 +0530

    feat: graceful quit, load config and initialize repositories, http handler route and rename the functions

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,62 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/CodeChefVIT/devsoc-backend-24/config"
+	"github.com/CodeChefVIT/devsoc-backend-24/internal/database"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func init() {
 	config.SanityCheck()
+	appConfig := config.LoadConfig()
+	database.InitDB(appConfig.DatabaseConfig)
+	database.InitialiseGoogleSheetsClient()
+	database.InitRedis(appConfig.RedisConfig)
 }
 
 func main() {
-	e := echo.New()
-	e.GET("/ping", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, "Pong")
+	app := echo.New()
+
+	app.Use(middleware.Logger())
+
+	app.GET("/ping", func(ctx echo.Context) error {
+		return ctx.JSON(http.StatusOK, map[string]string{
+			"message": "pong",
+			"status":  "successful start",
+		})
 	})
-	e.Logger.Fatal(e.Start(":8080"))
+
+	app.HTTPErrorHandler = func(err error, c echo.Context) {
+		code := http.StatusInternalServerError
+		message := "Not found"
+
+		if he, ok := err.(*echo.HTTPError); ok {
+			code = he.Code
+			message = he.Message.(string)
+		}
+
+		app.Logger.Error(err)
+		c.JSON(code, map[string]interface{}{
+			"status":  "false",
+			"code":    code,
+			"message": message,
+		})
+	}
+
+	// Graceful quit
+	c := make(chan os.Signal, 1)
+	go func() {
+		<-c
+		database.DB.Close()
+		_ = app.Shutdown(context.Background())
+	}()
+	signal.Notify(c, os.Interrupt)
+
+	app.Logger.Fatal(app.Start(":8080"))
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/CodeChefVIT/devsoc-backend-24
 go 1.21.5
 
 require (
+	github.com/google/uuid v1.4.0
 	github.com/labstack/echo/v4 v4.11.4
 	github.com/redis/go-redis/v9 v9.4.0
 	golang.org/x/oauth2 v0.15.0
@@ -15,10 +16,10 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
-	github.com/google/uuid v1.4.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
@@ -32,6 +33,7 @@ require (
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
 	google.golang.org/grpc v1.59.0 // indirect
