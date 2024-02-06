514d99bd47a425ab8e9f08922322914ced0f01f7
Black-Sirius69 <vedant.matanhelia@gmail.com>
2024-02-06 11:48:08 +0530

    feat: migrations

---
+++ db/migrations/001_users.sql
@@ -0,0 +1,24 @@
+-- +goose Up
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    phone_number TEXT NOT NULL,
+    college TEXT NOT NULL,
+    gender TEXT NOT NULL,
+    role TEXT NOT NULL,
+    country TEXT NOT NULL,
+    github TEXT NOT NULL,
+    bio TEXT NOT NULL,
+    is_banned BOOLEAN NOT NULL,
+    is_added BOOLEAN NOT NULL,
+    is_vitian BOOLEAN NOT NULL,
+    is_verified BOOLEAN NOT NULL,
+    team_id INT
+);
+
+-- +goose Down
+DROP TABLE users;
