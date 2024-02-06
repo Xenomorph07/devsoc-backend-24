72d5de92e2d863abaf1a497fd2e7072b5e9603b1
Aaditya Mahanta <aadityamahanta07@gmail.com>
2024-02-07 03:38:35 +0530

    feat: set up idea and project

---
+++ internal/controllers/project_controller.go
@@ -0,0 +1,15 @@
+package controllers
+
+import "github.com/labstack/echo/v4"
+
+func GetProject(ctx echo.Context) error {
+	return nil
+}
+
+func CreateProject(ctx echo.Context) error {
+	return nil
+}
+
+func UpdateProject(ctx echo.Context) error {
+	return nil
+}
---
+++ internal/routes/project_routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/CodeChefVIT/devsoc-backend-24/internal/controllers"
+	"github.com/CodeChefVIT/devsoc-backend-24/internal/middleware"
+	"github.com/labstack/echo/v4"
+)
+
+func ProjectRoutes(incomingRoutes *echo.Echo) {
+	project := incomingRoutes.Group("/project")
+	project.Use(middleware.Protected())
+	project.GET("/", controllers.GetProject)
+	project.POST("/create", controllers.CreateProject)
+	project.PATCH("/update", controllers.UpdateProject)
+}
